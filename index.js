e7190f5c543e9d2a109e70e8b7db8f1f743c46c6
Asapanna Rakesh <rakeshark22@gmail.com>
2020-08-01 23:57:45 +0530

    face recognition codebase functional. todo: add knn classifier / truncated mobnet

--- index.js
+++ index.js
@@ -11,26 +11,22 @@ async function setupCamera(videoElement) {
 }
 
 
-function scaleX(val){
-    // 100.35234832763672, 123.83369445800781 >> 162.0299072265625, 88.43865203857422
-    // 100*ratio = 162 (scaled) 
-    var ratio = 162.03 / 100.35
-    return val*ratio
-}
-function scaleY(val){
-    // 100.35234832763672, 123.83369445800781 >> 162.0299072265625, 88.43865203857422
-    // 123*ratio = 88 (scaled) 
-    var ratio = 88.44 / 123.83
-    return val*ratio
-}
+
 
 var loaded = false  
-function startRender(input, output, model) {
+function startRender(input, output, output2, model) {
     const ctx = output.getContext("2d");
+    const ctx2 = output2.getContext("2d");
+    
     async function renderFrame() {
         requestAnimationFrame(renderFrame);
         const faces = await model.estimateFaces(input, false, false);
         ctx.clearRect(0, 0, output.width, output.height);
+        ctx2.clearRect(0, 0, output2.width, output2.height);
+
+        ctx2.beginPath();
+        ctx2.drawImage(input, 0, 0, output2.width, output2.height)
+
         faces.forEach(face => {
 
             if (!loaded){
@@ -38,14 +34,43 @@ function startRender(input, output, model) {
                 loaded = true
             }
 
-            ctx.rect(
-                scaleX(face.boundingBox.topLeft[0][0]), 
-                scaleX(face.boundingBox.topLeft[0][1]), 
-                scaleY(face.boundingBox.bottomRight[0][0]), 
-                scaleY(face.boundingBox.bottomRight[0][1])
-                );
-            ctx.stroke();          
+            var cheekLen = face.annotations.leftCheek[0][0] - face.annotations.rightCheek[0][0]
+            var bboxHeight =  cheekLen * 3
+            var bboxWidth  =  cheekLen * 3    
+
+            var grad= ctx2.createLinearGradient(50, 50, 150, 150);
+            grad.addColorStop(0, "#00ff6a");
+            grad.addColorStop(1, "#4fc7ff");
+            ctx2.strokeStyle = grad;
+
+            /*ctx2.strokeRect(
+            face.annotations.noseTip[0][0] - (bboxWidth/2),
+            face.annotations.noseTip[0][1] - (bboxHeight/2),
+            bboxWidth, bboxHeight);
+            */
+            var tlx = face.annotations.noseTip[0][0] - (bboxWidth/2)
+            var tly = face.annotations.noseTip[0][1] - (bboxHeight/2)
             
+            ctx2.moveTo(tlx + (bboxWidth/3), tly);
+            ctx2.lineTo(tlx, tly);
+            ctx2.lineTo(tlx, tly + (bboxHeight/3));
+
+            ctx2.moveTo(tlx, tly + (2*bboxWidth/3));
+            ctx2.lineTo(tlx, tly + bboxWidth);
+            ctx2.lineTo(tlx + (bboxWidth/3), tly + bboxWidth);
+
+            ctx2.moveTo(tlx + (2*bboxWidth/3), tly);
+            ctx2.lineTo(tlx + bboxWidth, tly);
+            ctx2.lineTo(tlx + bboxWidth, tly + (bboxHeight/3));
+
+            ctx2.moveTo(tlx + bboxWidth, tly + (2*bboxHeight/3));
+            ctx2.lineTo(tlx + bboxWidth, tly + bboxHeight);
+            ctx2.lineTo(tlx + bboxWidth - (bboxWidth/3), tly + bboxHeight);
+
+            ctx2.stroke();
+            
+
+
             face.scaledMesh.forEach(xy => {
                 ctx.beginPath();
                 ctx.arc(xy[0], xy[1], 1, 0, 2 * Math.PI);
@@ -55,22 +80,6 @@ function startRender(input, output, model) {
         });
     }
     renderFrame();
-}
-
-function startBlaze(input, output, model) {
-    const ctx = output.getContext("2d");
-    async function renderFrame() {
-        requestAnimationFrame(renderFrame);
-        const preds = await model.estimateFaces(input, false); // retTensor=false
-        console.log(preds)
-        preds.forEach(face => {
-            ctx.rect(face.topLeft[0], face.topLeft[1], face.bottomRight[0], face.bottomRight[1]);
-            ctx.stroke(); 
-        })
-
-        ctx.clearRect(0, 0, output.width, output.height);
-    }
-    renderFrame()
 }
 
 
@@ -88,9 +97,7 @@ async function start() {
     
     await setupCamera(input);
     const model = await facemesh.load({ maxFaces: 1 });
-    const blaze = await blazeface.load();
-    startRender(input, output, model);
-    startBlaze(input, output2, blaze);
+    startRender(input, output, output2, model);
 
     loading(false, "loadingicon");
     loading(false, "loadingicon2");
